3e05f31654052284b9e1ee972bfbf65eceda87aa
SirRichie <tobias.fr@gmail.com>
2015-05-05 23:07:06 +0200

    intermediate commit to merge

---
+++ MalifoApp/MalifoApp/ViewModels/CardViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Common.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalifoApp.ViewModels
+{
+    /// <summary>
+    /// A ViewModel for a Card
+    /// </summary>
+    public class CardViewModel : ViewModel<Card>
+    {
+        public CardViewModel(Card model) : base(model) { }
+
+        public string Text
+        {
+            get
+            {
+                return Model.Text;
+            }
+            set
+            {
+                Model.Text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+    }
+}
---
+++ MalifoApp/MalifoApp/ViewModels/DeckViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Common.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalifoApp.ViewModels
+{
+    public class DeckViewModel : ViewModel
+    {
+        private Deck 
+    }
+}
