0461d25036d5bfaeb4f2e46c6d2a223c1424168d
grizzly885 <dsgrizzly@gmail.com>
2015-05-05 17:27:55 +0200

    Add Common Project

---
+++ MalifoApp/Common/types/exceptions/BusinessException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.types.exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException()
+            : base()
+        {
+
+        }
+
+        public BusinessException(string msg)
+            : base(msg)
+        {
+
+        }
+
+        public BusinessException(string msg, Exception e)
+            : base(msg, e)
+        {
+
+        }
+    }
+}
