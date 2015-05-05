48ed6f70ce276c932a5b1d8140f4a271782a3216
SirRichie <tobias.fr@gmail.com>
2015-05-05 19:32:11 +0200

    added model for Deck and Card

--- MalifoApp/Common/models/Card.cs
+++ MalifoApp/Common/models/Card.cs
@@ -8,6 +8,14 @@ namespace Common.models
 {
     public class Card
     {
+        public string Text {get; set;}
+        public byte[] Image { get; set; }
 
+        public Card() { }
+
+        public Card(string text)
+        {
+            this.Text = text;
+        }
     }
 }
