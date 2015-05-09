b43bbe2224b6b31a9d5319f02e4d5003ddc79ccf
SirRichie <tobias.fr@gmail.com>
2015-05-09 19:57:22 +0200

    cleaned up UI prepared UI for full game state display

--- MalifoApp/MalifoApp/Properties/Resources.Designer.cs
+++ MalifoApp/MalifoApp/Properties/Resources.Designer.cs
@@ -93,6 +93,26 @@ namespace MalifoApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap facedown {
+            get {
+                object obj = ResourceManager.GetObject("facedown", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap faceup {
+            get {
+                object obj = ResourceManager.GetObject("faceup", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap fourcards {
             get {
                 object obj = ResourceManager.GetObject("fourcards", resourceCulture);
